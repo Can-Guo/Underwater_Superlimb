15c3471ee534368f414dda1854cd5f65d7198e30
Yuqin Guo <12032421@mail.sustech.edu.cn>
2022-08-30 23:28:35 +0800

    20220830-4 test is ok: send imu_data string from server to client!

--- python/script/socket_imu_server.py
+++ python/script/socket_imu_server.py
@@ -1,8 +1,8 @@
 '''
 Date: 2022-08-27 00:44:09
 LastEditors: Guo Yuqin,12032421@mail.sustech.edu.cn
-LastEditTime: 2022-08-30 23:12:31
-FilePath: \script\socket_imu_server.py
+LastEditTime: 2022-08-30 23:24:56
+FilePath: /script/socket_imu_server.py
 '''
 
 
@@ -58,7 +58,7 @@ class IMUSocketClass(object):
     ## 
 
     def encode_imu_data_to_str(self, number_list):
-        send_string = []
+        send_string = str('')
         for data in number_list:
             send_string = send_string + str(data) + ','
 
@@ -76,7 +76,7 @@ class IMUSocketClass(object):
         return 
 
 
-    def create_csv():
+    def create_csv(self):
         cwd = os.path.abspath('.')
         cwd = os.path.dirname(cwd)
 
@@ -119,17 +119,18 @@ filename = IMUSocket.create_csv()
 imu_data_frame = Microstrain_IMU.parseDataStream_Number(100,1,accel_enable,euler_enable)
 
 Send_String = IMUSocket.encode_imu_data_to_str(imu_data_frame)
+Send_String = Send_String.encode('utf-8')
 
 print("UDP Server Communication Begin!")
 
-# while(True):
+while(True):
 
-Data = IMUSocket.UDPServerSocket.recvfrom(1024)
+    Data = IMUSocket.UDPServerSocket.recvfrom(1024)
 
-print("Client IP Address:{}".format(Data[1]))
-print("Message from Client:{}".format(Data[0].decode('gbk')))
+    print("Client IP Address:{}".format(Data[1]))
+    print("Message from Client:{}".format(Data[0].decode('gbk')))
     
-IMUSocket.UDPServerSocket.sendto(Send_String, Data[1])
+    IMUSocket.UDPServerSocket.sendto(Send_String, Data[1])
 
 IMUSocket.UDPServerSocket.close()
 
