eba323582c7a7372fa68322991eb3179d8d98b21
Can-Guo <1739950387@qq.com>
2022-08-30 22:39:27 +0800

    20220830-2 modified to differ the system platform for different file path system

--- python/script/test_method.py
+++ python/script/test_method.py
@@ -1,7 +1,7 @@
 '''
 Date: 2022-08-30 21:58:32
 LastEditors: Guo Yuqin,12032421@mail.sustech.edu.cn
-LastEditTime: 2022-08-30 22:15:46
+LastEditTime: 2022-08-30 22:36:41
 FilePath: \script\test_method.py
 '''
 
@@ -9,18 +9,32 @@ FilePath: \script\test_method.py
 from datetime import datetime 
 import os 
 import csv 
+import platform 
+
+
+if platform.system().lower() == 'windows':
+    print("当前的操作系统是windows")
+    sys_platform = 'windows'
+elif platform.system().lower() == 'linux':
+    print("当前的操作系统是linux")
+    sys_platform = 'linux'
+
+print(sys_platform)
 
 
 def create_csv():
     cwd = os.path.abspath('.')
     dir = os.path.dirname(cwd)
 
-    time_now = datetime.now()
+    time_mark = datetime.now()
 
-    time_mark = time_now.strftime("%Y-%m-%d %H-%M-%S")
-    file_name = str(dir) + '\csv\imu_data_' + str(time_mark) + '_save.csv'
+    time_mark = time_mark.strftime("%Y-%m-%d %H-%M-%S")
+    if sys_platform == 'windows':
+        file_name = str(dir) + '\csv\imu_data_' + str(time_mark) + '_save.csv'
+    elif sys_platform == 'linux':
+        file_name = str(dir) + '/csv/imu_data_' + str(time_mark) + '_save.csv'
 
-    print(file_name)
+    # print(file_name)
 
     with open(file_name, 'a') as file:
         writer = csv.writer(file, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
