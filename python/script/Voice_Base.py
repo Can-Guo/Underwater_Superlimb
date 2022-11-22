873f35c1286909e1c01e8d39984744c58f75f02b
Can-Guo <1739950387@qq.com>
2022-11-22 15:21:41 +0800

    20221122-0 modified Voice-base for testing in linux

--- python/script/Voice_Base.py
+++ python/script/Voice_Base.py
@@ -1,21 +1,35 @@
 '''
 Date: 2022-11-14 16:28:57
 LastEditors: Guo Yuqin,12032421@mail.sustech.edu.cn
-LastEditTime: 2022-11-14 17:26:07
-FilePath: \script\Voice_Base.py
+LastEditTime: 2022-11-22 11:07:14
+FilePath: \python\script\Voice_Base.py
 '''
+
 import wave 
 import pyaudio 
+from scipy.io import wavfile
+import matplotlib.pyplot as plt 
+import numpy as np 
+
 
 class Voice_Base(object):
     
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, path=[]):
+        self.pa = pyaudio.PyAudio()
+        
+        for i in range(self.pa.get_device_count()):
+            self.device_info = self.pa.get_device_info_by_index(i)
+            print(self.device_info)
+            device_index= self.device_info.get('index')
+        
+        print(device_index)
+
+        # self.path = path
 
     
-    def audioreorder(self, len=2, formater = pyaudio.paInt16, rate = 16000, frames_per_buffer=1024, channels=2):
+    def audiorecorder(self, len=2, formater = pyaudio.paInt16, rate = 16000, frames_per_buffer=1024, channels=2):
         """
-        【函数功能：使用麦克风进行录音操作】
+        函数功能：使用麦克风进行录音操作
 
         Input Param:
             len:录制时间长度(秒)
@@ -26,8 +40,9 @@ class Voice_Base(object):
         Return:
             None
         """
-        pa = pyaudio.PyAudio()
-        stream = pa.open(format=formater, channels=channels, rate=rate, input=True, frames_per_buffer=frames_per_buffer)
+        
+        
+        stream = self.pa.open(format=formater, channels=channels, rate=rate, input=True, frames_per_buffer=frames_per_buffer)
         print("Microphone is Recording……")
         frames = []
 
@@ -40,19 +55,20 @@ class Voice_Base(object):
         stream.stop_stream()
         stream.close()
 
-        pa.terminate()
+        self.pa.terminate()
 
         wf = wave.open(self.path, "wb")
         wf.setnchannels(channels)
-        wf.setsampwidth(pa.get_sample_size(formater))
+        wf.setsampwidth(self.pa.get_sample_size(formater))
         wf.setframerate(rate)
         wf.writeframes(b''.join(frames))
         wf.close()
     
+
     def audiowrite(self, data, fs, binary = True, channel = 1, path=[]):
 
         """
-        【函数功能：语音信息写入到.wav文件】
+        函数功能：语音信息写入到.wav文件
         Input Param:
             data: 语音信号数据
             fs:采样率(Hz)
@@ -65,4 +81,146 @@ class Voice_Base(object):
 
         if len(path) == 0:
             path = self.path
-        if 
+        if binary:
+            wf = wave.open(path,'wb')
+            wf.setframerate(fs)
+            wf.setnchannels(channel)
+            wf.setsampwidth(2)
+            wf.writeframes(b''.join(data))
+        else:
+            wavfile.write(path,fs,data)
+    
+
+    def audioread(self, return_nbits=False, formater='sample'):
+        """
+        函数功能:读取语音文件,返回语音数据data,采样率fs,数据位数bits
+        Input Param:
+            formater:获取数据的格式,为sample时,数据为float32的[-1,1],
+                     否则为文件本身的数据格式指定formater为任意非sample字符串,则返回原始数据
+        Return:
+            data: 语音数据
+            fs: 采样率
+            bits: 数据位数
+        """
+
+        fs, data, bits = wavfile.read(self.path)
+
+        if formater == 'sample':
+            data = data/(2**(bits-1))
+        if return_nbits:
+            return data, fs, bits 
+        else:
+            return data, fs
+    
+
+    def soundplot(self, data=[], samplerate=16000, size=(14,5)):
+        """
+        函数功能:将语音数据/或读取语音数据并绘制成图
+        Input Param:
+            data: 语音数据
+            samplerate: 采样率
+            size: 绘图窗口大小
+        Return:
+            None
+        """
+        if len(data) == 0:
+            data, fs, _= self.audioread()
+        plt.figure(figsize=size)
+        x = [i/ samplerate for i in range(len(data))]
+        plt.plot(x,data)
+        plt.xlim([0, len(data) / samplerate])
+        plt.xlabel('time/sec')
+        plt.show()
+
+    def soundadd(self, data1, data2):
+        """
+        函数功能: 将两个语音信号序列相加,若长短不一,在短的序列后端补零
+        Input Param: 
+            data1: 语音数据序列1
+            data2: 语音数据序列2
+        Return: 
+
+        """
+        if len(data1) < len(data2):
+            temp = np.zeros([len(data2)])
+            for i in range(len(data1)):
+                temp[i] += data1[i]
+            return temp + data2 
+
+        elif len(data1) > len(data2):
+            temp = np.zeros([len(data1)])
+            for i in range(len(data2)):
+                temp[i] += data2[i]
+            return temp + data1 
+        else:
+            return data1 + data2
+
+
+    def SPL_calculate(self, data, fs, frameLen):
+        """
+        函数功能: 根据数学公式计算单个声压值 $ y=\sqrt( \sum_{i=1} ^ Nx^2(i) ) $
+        Input Param:
+            data: 输入数据
+            fs: 采样率
+            frameLen: 计算声压的时间长度(ms单位)
+        Return:
+            单个声压数值
+        """
+        l = len(data)
+        M = frameLen * fs / 1000
+        if not 1 == M:
+            exit('输入信号长度与所定义帧长不等!请检查!')
+        # 计算有效声压
+        pp = 0
+        for i in range(int(M)):
+            pp += (data[i] * data[i])
+        pa = np.sqrt(pp / M)
+        p0 = 2e-5
+        SPL = 20 * np.log10(pa / p0)
+
+        return SPL 
+    
+    
+    def SPL(self, data, fs, framLen=100, isplot=True):
+        """
+        计算声压曲线    
+        Input Param:
+            data: 语音信号数据
+            fs: 采样率
+            frameLen: 计算声压的时间长度(ms单位)
+            isplot: 是否绘图，默认是
+        Return: 
+            返回声压列表spls
+        """
+
+        length = len(data)
+        M = fs * framLen // 1000
+        m = length % M 
+        if not m < M // 2:
+            # 最后一帧长度不小于M的一半
+            data = np.hstack((data, np.zeros(M - m)))
+        else:
+            # 最后一帧长度小于M的一半
+            data = data[:M * (length // M)]
+        spls = np.zeros(len(data) // M)
+        for i in range(len(data) // M - 1):
+            s = data[i * M : (i+1) * M]
+            spls[i] = self.SPL_calculate(s, fs, framLen)
+        
+        if isplot:
+            plt.subplot(211)
+            plt.plot(data)
+            plt.subplot(212)
+            plt.step([i for i in range(len(spls))], spls)
+            plt.show()
+        
+        return spls 
+
+###############
+# Test the Class Methods
+AU = Voice_Base()
+
+###############
+    
+
+            
