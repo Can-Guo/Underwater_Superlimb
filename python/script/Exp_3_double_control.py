01822e218fb6afd299b99f084ee3e59f8f91c9de
Can-Guo <1739950387@qq.com>
2023-03-14 23:02:03 +0800

    20230314-3 from Minotebook, save code for exp3, tested succeed.

--- python/script/Exp_3_double_control.py
+++ python/script/Exp_3_double_control.py
@@ -1,19 +1,26 @@
 '''
-Date: 2023-03-10 20:10:13
+Date: 2023-03-14 15:20:45
 LastEditors: Guo Yuqin,12032421@mail.sustech.edu.cn
-LastEditTime: 2023-03-14 15:42:17
-FilePath: /script/Exp_1_imu_control.py
+LastEditTime: 2023-03-14 22:25:59
+FilePath: /script/Exp_3_double_control.py
 '''
+
 
 import socket as Socket
 
 ## socket TCP: ubuntu(server) <==> Raspberry Pi 4B
-port = 3300
-## socket TCP : Ubuntu(server) <==> Raspberry Pi 4B
+port = 3366
 s= Socket.socket(Socket.AF_INET, Socket.SOCK_STREAM)
 s.bind(("10.12.234.126",port))
 s.listen(10)
 client_socket,clienttAddr=s.accept()
+
+## socket TCP : Ubuntu(client) <==> Mi-notebook (server)
+import socket as Socket
+socket_Mi = Socket.socket(Socket.AF_INET, Socket.SOCK_STREAM)
+socket_Mi.settimeout(5000)
+socket_Mi.connect(("10.13.228.137", 8888))
+
 
 ## import libs
 import time
@@ -34,6 +41,16 @@ roll_before = 0.0
 pitch_before = 0.0
 yaw_before = 0.0
 
+
+def decodeMI(data_mi_byte):
+    
+    data_mi = data_mi_byte.decode('utf-8')
+
+    data_mi_list = data_mi.split(',')
+
+    return data_mi_list
+
+
 ## Method 1: read CSV file
 
 def readIMUCSV(csv_file, flag):
@@ -52,12 +69,10 @@ def readIMUCSV(csv_file, flag):
     elif flag == 2:
         frame_number = len(data_frame['Timestamp_cmd'])
 
-    # global last_frame_number
-    # last_frame_number.append(frame_number)
-
     print("Frame Number: %d \r\n" % frame_number)
 
     return data_frame
+
 
 def createCommandCSV(file_name):
     cwd = os.path.abspath('.')
@@ -67,27 +82,27 @@ def createCommandCSV(file_name):
     
     with open(file_name, 'a') as file:
         writer = csv.writer(file, delimiter = ',', quotechar='"', quoting = csv.QUOTE_MINIMAL)
-        writer.writerow(['Timestamp_cmd','imu_cmd','roll_delta','pitch_delta','yaw_delta'])
+        writer.writerow(['Timestamp_cmd','imu_cmd','roll_delta','pitch_delta','yaw_delta','voice_type','voice_length'])
 
     file.close()
     
     return file_name
 
 
-## Encode IMU Message/ Token into string Message to be send
-def encodeIMU_control(next_state,current_roll,current_pitch, current_yaw):
+## Encode IMU and Throat Vibration Message/ Token into string Message to be send
+def encodeDoublecontrol(next_state,current_roll,current_pitch, current_yaw, voice_type, voice_length):
     # send_string = str('')
     current_roll = round(current_roll,2)
     current_pitch = round(current_pitch,2)
     current_yaw = round(current_yaw,2)
-    send_string = str(next_state) + ',' + str(current_roll) +',' + str(current_pitch) + ',' + str(current_yaw) + '!'
+    send_string = str(next_state) + ',' + str(current_roll) +',' + str(current_pitch) + ',' + str(current_yaw) + '#' + str(voice_type) + ',' + str(voice_length) + '!'
 
-    print("Encoded Data: %s " % send_string)
+    # print("Encoded Data: %s " % send_string)
 
     return send_string
 
 
-def IMU_control(csv_imu_file, csv_command_file):
+def Double_control(csv_imu_file, csv_command_file):
 
     ## calibration of the IMU sensor
     print("Please Wait until the IMU data is stable for 5 seconeds!\r\n")
@@ -99,6 +114,8 @@ def IMU_control(csv_imu_file, csv_command_file):
     global yaw_before
 
     IMU_command_label = ['0','1','2','3','4','5','6']
+    imu_last_mode = 0
+    voice_last_mode = 'Q'
     next_mode = IMU_command_label[0]
 
     X_threshold = 20.0 #/180.0 * np.pi 
@@ -114,6 +131,15 @@ def IMU_control(csv_imu_file, csv_command_file):
     timestamp_frame_one = Data_list_one[:,0]
 
     print("Client continue...")
+    
+
+    # try:
+    #     Data_Mi = socket_Mi.recv(1024)
+        
+    # except BlockingIOError as e:
+    #     print("Error!",e)
+    
+
 
     if len(last_frame_number) == 1:
 
@@ -131,26 +157,29 @@ def IMU_control(csv_imu_file, csv_command_file):
         for i in range(last_frame_number[0]):
     
             with open(csv_command_file, 'a') as file:
-                writer = csv.DictWriter(file, fieldnames=['Timestamp_cmd','imu_cmd','roll_delta','pitch_delta','yaw_delta']) #,'Throat_Mi'])
+                writer = csv.DictWriter(file, fieldnames=['Timestamp_cmd','imu_cmd','roll_delta','pitch_delta','yaw_delta',
+                                                          'voice_type','voice_length'])
                 writer.writerow({'Timestamp_cmd':timestamp_frame_one[i],'imu_cmd':next_mode, 
                                  'roll_delta':roll_record_before[i],
-                                 'pitch_delta':pitch_record_before[i],'yaw_delta':yaw_record_before[i]})
+                                 'pitch_delta':pitch_record_before[i],
+                                 'yaw_delta':yaw_record_before[i],
+                                 'voice_type':'Q','voice_length':'Q'})
             
             file.close()
 
         print("Last Frame Number: %d", last_frame_number)
 
         # Socekt Message Sending
-        Send_string = encodeIMU_control(next_mode, roll_before, pitch_before, yaw_before)
+        Send_string = encodeDoublecontrol(next_mode, roll_before, pitch_before, yaw_before,'Q','Q')
         client_socket.send(Send_string.encode('utf-8'))
 
         print("send for Once!\r\n") 
 
         print("Calibration is Done! Please begin control!\r\n")
 
-
     while(True):
-        time.sleep(0.001)
+
+        time.sleep(0.01)
         IMU_data_frame = readIMUCSV(csv_imu_file,1)
 
         Data_list = np.array(IMU_data_frame)
@@ -159,9 +188,18 @@ def IMU_control(csv_imu_file, csv_command_file):
 
         timestamp_frame = Data_list[:,0]
 
+        try:
+            Data_Mi = socket_Mi.recv(1024)
+            
+        except BlockingIOError as e:
+            print("Error!",e)
+            
         if len(last_frame_number) >= 2:
 
             for i in range(last_frame_number[-2],last_frame_number[-1]):
+
+
+
 
                 if Data_list.shape[1] >= 4:
                     roll_record_new = Data_list[last_frame_number[-2]:last_frame_number[-1],4]
@@ -197,21 +235,40 @@ def IMU_control(csv_imu_file, csv_command_file):
                     and (np.abs(roll_record_new[i-last_frame_number[-2]] - roll_before) <= np.abs(0.5*X_threshold))):
                     next_mode = IMU_command_label[0]
                     pass 
+                    
+                # last_mode = next_mode
+                
+                # if(decodeMI(Data_Mi)[0]=='do')
+                
 
                 with open(csv_command_file, 'a') as file:
-                    writer = csv.DictWriter(file, fieldnames=['Timestamp_cmd','imu_cmd','roll_delta','pitch_delta','yaw_delta'])
+                    writer = csv.DictWriter(file, fieldnames=['Timestamp_cmd','imu_cmd','roll_delta','pitch_delta','yaw_delta','voice_type','voice_length'])
                     writer.writerow({'Timestamp_cmd':timestamp_frame[i],'imu_cmd':next_mode,'roll_delta':roll_record_new[i-last_frame_number[-2]],
-                                     'pitch_delta':pitch_record_new[i-last_frame_number[-2]],'yaw_delta':yaw_record_new[i-last_frame_number[-2]]})
-                
+                                     'pitch_delta':pitch_record_new[i-last_frame_number[-2]],'yaw_delta':yaw_record_new[i-last_frame_number[-2]],
+                                     'voice_type':decodeMI(Data_Mi)[0],'voice_length':decodeMI(Data_Mi)[1]})
                 file.close()  
-            
-                Send_string = encodeIMU_control(next_mode,
-                                                (roll_record_new[i-last_frame_number[-2]] - roll_before),
-                                                (pitch_record_new[i-last_frame_number[-2]] - pitch_before),
-                                                (yaw_record_new[i-last_frame_number[-2]]- yaw_before))
+
+                if (voice_last_mode==decodeMI(Data_Mi)[0]) and (decodeMI(Data_Mi)[0]=='do' or decodeMI(Data_Mi)[0]=='re' or decodeMI(Data_Mi)[0]=='me' or decodeMI(Data_Mi)[0]=='fa'):
+                    break
+
+                if (voice_last_mode == 'fa' and next_mode == imu_last_mode and decodeMI(Data_Mi)[0]=='Q'):
+                    print("Don't send!")
+                    break
+
+                imu_last_mode = next_mode
+
+                voice_last_mode = decodeMI(Data_Mi)[0]
+
+                Send_string = encodeDoublecontrol(next_mode,
+                                (roll_record_new[i-last_frame_number[-2]] - roll_before),
+                                (pitch_record_new[i-last_frame_number[-2]] - pitch_before),
+                                (yaw_record_new[i-last_frame_number[-2]]- yaw_before),
+                                decodeMI(Data_Mi)[0],decodeMI(Data_Mi)[1])
                 
                 client_socket.send(Send_string.encode('utf-8'))
+                print("Sent Message:", Send_string)
 
+       
 
 def plot_csv_cmd(csv_command_file):
     
@@ -259,23 +316,21 @@ def take_str_name(flag):
 
 if __name__ == '__main__':
     
-    record_or_plot = 2
+    record_or_plot = 1
 
     ## Record and transmitte
     if record_or_plot==1:
         file_name = take_str_name(flag=1)
         print(file_name)
         command_file = createCommandCSV(file_name)
-        IMU_control(csv_command_file=command_file, csv_imu_file=file_name)
+        Double_control(csv_command_file=command_file, csv_imu_file=file_name)
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
     # ## Test the results
     elif record_or_plot==2:
         file_name = take_str_name(flag=2)
         print(file_name)
         plot_csv_cmd(file_name)
-    
-
 
 else:
+    
     print("Errors! Please check your codes.\r\n")
-
